3b25ffbc6c84684029a880c92956070613b2e37d
hangsiahong <honghangsia@gmail.com>
2024-06-14 10:49:10 +0700

    add handle exception

--- index.ts
+++ index.ts
@@ -28,3 +28,6 @@ const HOST = process.env.HOST || '0.0.0.0';
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
+
+process.on("unhandledRejection", (error) => {})
+process.on("uncaughtExeption", (error) => {})
