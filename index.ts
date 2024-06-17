5a458e7f428a6bf4d366e8744a3aa6ecf66d2682
hangsiahong <honghangsia@gmail>
2024-06-17 23:57:04 +0700

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import session from 'express-session';
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import vmRoutes from "./routes/vmRoutes";
+import { initializeClient } from "./client/vinsta/cmd/initVinsta";
 
 dotenv.config();  // Load environment variables from .env file
 
@@ -36,6 +38,14 @@ const connectDB = async () => {
 connectDB();
 
 app.use("/api", vmRoutes);
+// Assume '/initialize' is your route for client initialization
+app.get('/initialize', async (req, res) => {
+  // Pass req to initializeClient function
+  await initializeClient(req);
+
+  // Respond to the client as needed
+  res.send('Initialization complete'); 
+});
 app.use(express.static(__dirname + '/public'));
 
 const PORT = Number(process.env.PORT || 3333);
@@ -44,6 +54,17 @@ const HOST = process.env.HOST || '0.0.0.0';
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
+
+// Setup express-session middleware
+app.use(session({
+  secret: '123456789', // Testing secret only
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // Set to true if using HTTPS
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours (session expiration time)
+  },
+}));
 
 process.on("unhandledRejection", (error) => {
   console.error("Unhandled Rejection:", error);
